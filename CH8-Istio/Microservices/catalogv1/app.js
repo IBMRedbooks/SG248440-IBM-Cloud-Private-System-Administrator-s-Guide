d4e3bde3ef3ec590c7c55883a9ff22886c1f630f
sanju2312 <38077979+sanju2312@users.noreply.github.com>
2019-06-25 18:53:43 +0530

    test

---
+++ CH8-Istio/Microservices/catalogv1/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var healthy=true;
+var url='http://'+process.env.PRODUCT_SERVICE_HOST+':'+process.env.PRODUCT_SERVICE_PORT;
+app.get('/', function (req, res) {
+   
+   var request = require('request');
+request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200&&healthy) {
+        res.send(200,"user==>catalog:v1==>product:"+body);
+     }
+     else
+     res.send(503,'catalog:v1 service not available');
+})
+});
+
+app.get('/unhealthy', function(req,res){
+ healthy=false;
+ res.send('service got unhealthy');
+ });
+
+var server = app.listen(8000, function () {
+  
+})
